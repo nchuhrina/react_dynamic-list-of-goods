48227e49608769f3954fe54771722ccad86116ca
Nina <pinkangelssugar@gmail.com>
2020-05-12 22:35:55 +0300

    Updates

---
+++ precache-manifest.c08e9d051e4960b25b146614f5c66bac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263b2271c928dfcdbaae93d81edd6da9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1abaa3280fb1aa4a8fa1",
+    "url": "./static/css/main.ba023361.chunk.css"
+  },
+  {
+    "revision": "acf890f14034b7345c62",
+    "url": "./static/js/2.84b77ff7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.84b77ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1abaa3280fb1aa4a8fa1",
+    "url": "./static/js/main.49366fe5.chunk.js"
+  },
+  {
+    "revision": "cc951e334f15251cfce1",
+    "url": "./static/js/runtime-main.88190073.js"
+  }
+]);
